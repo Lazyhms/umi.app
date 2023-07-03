89a1e35c1c4128c754a1e2a4baca7ce882ea11cb
hu15150 <hu15150@126.com>
2023-07-03 15:36:31 +0800

    remove some

--- src/pages/Core/Role/index.tsx
+++ src/pages/Core/Role/index.tsx
@@ -1,4 +1,4 @@
-import RemoteSelect, { RemoteOption } from '@/components/Select/RemoteSelect';
+import RemoteSelect, { RemoteOption } from '@/components/Select/RemoteSelect.d';
 import { useRef } from 'react';
 
 const TableList: React.FC<unknown> = () => {
@@ -6,34 +6,32 @@ const TableList: React.FC<unknown> = () => {
   const childRef = useRef<RemoteOption>(null);
 
   return (
-    <>
-      <RemoteSelect
-        options={[
-          {
-            label: "请选择",
-            value: undefined
-          }
-        ]}
-        ref={childRef}
-        remote={{
-          url: '/api/dorpDown/roles',
-          pageSize: 10,
-          responseInterceptors: (e: any) => {
-            e.data.data = e.data.data.map((m: any) => {
-              return {
-                ...m,
-                label1: m.label,
-              }
-            });
-            return e;
-          }
-        }}
-        onSelect={(e) => {
-          const item = childRef.current?.option?.find((f: any) => f.value === e);
-          console.log(item);
-        }}
-      ></RemoteSelect>
-    </>
+    <RemoteSelect
+      options={[
+        {
+          label: "请选择",
+          value: undefined
+        }
+      ]}
+      ref={childRef}
+      remote={{
+        url: '/api/dorpDown/roles',
+        pageSize: 10,
+        responseInterceptors: (e: any) => {
+          e.data.data = e.data.data.map((m: any) => {
+            return {
+              ...m,
+              label1: m.label,
+            }
+          });
+          return e;
+        }
+      }}
+      onSelect={(e) => {
+        const item = childRef.current?.option?.find((f: any) => f.value === e);
+        console.log(item);
+      }}
+    ></RemoteSelect>
   );
 };
 
